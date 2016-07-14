eb1bbdd2d79cd762ca82a80cc81aaea4bd55ec00
Jeevesh Pandey <jeevesh.pandey@tothenew.com>
2016-07-14 13:15:34 +0530

    Identity

--- src/main/java/sample/domain/User.java
+++ src/main/java/sample/domain/User.java
@@ -1,13 +1,12 @@
 package sample.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column
     private String email;
