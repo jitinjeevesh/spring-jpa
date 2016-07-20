224fbe097891f23449f7ff2f22a90e005631586f
Jeevesh Pandey <jeevesh.pandey@tothenew.com>
2016-07-20 13:31:00 +0530

    Modification:User defined repository

--- src/main/java/sample/controller/UserController.java
+++ src/main/java/sample/controller/UserController.java
@@ -1,6 +1,7 @@
 package sample.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sample.dao.UserDao;
@@ -38,5 +39,10 @@ public class UserController {
     @RequestMapping("list")
     public List<User> list() {
         return userDao.list();
+    }
+
+    @RequestMapping("findByName/{name}")
+    public User findByName(@PathVariable String name) {
+        return userRepository.findByEmail(name);
     }
 }
--- src/main/java/sample/repository/UserRepository.java
+++ src/main/java/sample/repository/UserRepository.java
@@ -3,5 +3,13 @@ package sample.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sample.domain.User;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+
+    User findByName(String name);
+
+    List<User> findAllByName(String name);
 }
