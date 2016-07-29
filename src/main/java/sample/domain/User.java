ede35d336f97ca3c9902d43c5fe5f360b27d31d8
Jeevesh Pandey <jeevesh.pandey@tothenew.com>
2016-07-29 18:14:00 +0530

    Modification:One to Many

--- src/main/java/sample/domain/User.java
+++ src/main/java/sample/domain/User.java
@@ -1,11 +1,12 @@
 package sample.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
-@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name="DISC", discriminatorType=DiscriminatorType.STRING)
-@DiscriminatorValue(value="USER")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "DISC", discriminatorType = DiscriminatorType.STRING)
+@DiscriminatorValue(value = "USER")
 @NamedQuery(name = "User.findByNameNamed", query = "SELECT u FROM User u WHERE LOWER(u.name) = LOWER(?1)")
 @Table(name = "user")
 public class User {
@@ -17,6 +18,8 @@ public class User {
     private String email;
     @Column(name = "full_name", nullable = false, length = 25)
     private String name;
+    @OneToMany(mappedBy = "user")
+    private Set<Account> accounts;
 
     public Long getId() {
         return id;
@@ -40,5 +43,13 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(Set<Account> accounts) {
+        this.accounts = accounts;
     }
 }
